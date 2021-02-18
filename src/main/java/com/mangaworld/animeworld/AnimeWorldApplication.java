a169cc18e361ed013e4a35d8835ab7f5f23b80b2
acontreras15 <79065673+acontreras15@users.noreply.github.com>
2021-02-18 13:01:48 -0600

    Initial commit

---
+++ src/main/java/com/mangaworld/animeworld/AnimeWorldApplication.java
@@ -0,0 +1,13 @@
+package com.mangaworld.animeworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mangaworld/animeworld/AnimeWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangaworld.animeworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
